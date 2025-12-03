44a0099b1f0bb981086aed5da23cc3949fd3092e
AlexClark <alexsantossilva44@gmail.com>
2025-12-03 19:26:51 +0000

    Implementação inicial do programa Super Trunfo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,132 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+typedef struct Carta {
+    char estado;
+    char codigo[10];
+    char cidade[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+} Carta;
+
+void lerString(char *destino, int tamanho) {
+    fgets(destino, tamanho, stdin);
+    destino[strcspn(destino, "\n")] = '\0'; // remove o \n
+}
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+    Carta c1, c2;
+    char buffer[50];
 
+    printf("=== Cadastro da Carta 1 ===\n");
+
+    printf("Estado (A-H): ");
+    scanf(" %c", &c1.estado);
+    getchar(); // limpar o \n
+
+    printf("Codigo da carta (ex. A01 ... H04): ");
+    lerString(c1.codigo, sizeof(c1.codigo));
+
+    printf("Cidade: ");
+    lerString(c1.cidade, sizeof(c1.cidade));
+
+    printf("Populacao: ");
+    lerString(buffer, sizeof(buffer));
+    c1.populacao = atoi(buffer);
+
+    printf("Area (Km2): ");
+    lerString(buffer, sizeof(buffer));
+    c1.area = atof(buffer);
+
+    printf("PIB: ");
+    lerString(buffer, sizeof(buffer));
+    c1.pib = atof(buffer);
+
+    printf("Pontos turisticos: ");
+    lerString(buffer, sizeof(buffer));
+    c1.pontos_turisticos = atoi(buffer);
+
+    printf("\n=== Cadastro da Carta 2 ===\n");
+
+    printf("Estado (A-H): ");
+    scanf(" %c", &c2.estado);
+    getchar(); // limpar o \n
+
+    printf("Codigo da carta (ex. A01 ... H04): ");
+    lerString(c2.codigo, sizeof(c2.codigo));
+
+    printf("Cidade: ");
+    lerString(c2.cidade, sizeof(c2.cidade));
+
+    printf("Populacao: ");
+    lerString(buffer, sizeof(buffer));
+    c2.populacao = atoi(buffer);
+
+    printf("Area (Km2): ");
+    lerString(buffer, sizeof(buffer));
+    c2.area = atof(buffer);
+
+    printf("PIB: ");
+    lerString(buffer, sizeof(buffer));
+    c2.pib = atof(buffer);
+
+    printf("Pontos turisticos: ");
+    lerString(buffer, sizeof(buffer));
+    c2.pontos_turisticos = atoi(buffer);
+
+    printf("\n=== CARTA 1 CADASTRADA ===\n");
+    printf("Estado: %c\n", c1.estado);
+    printf("Codigo: %s\n", c1.codigo);
+    printf("Cidade: %s\n", c1.cidade);
+    printf("Populacao: %d\n", c1.populacao);
+    printf("Area: %.2f Km2\n", c1.area);
+    printf("PIB: %.2f\n", c1.pib);
+    printf("Pontos Turisticos: %d\n", c1.pontos_turisticos);
+
+    printf("\n=== CARTA 2 CADASTRADA ===\n");
+    printf("Estado: %c\n", c2.estado);
+    printf("Codigo: %s\n", c2.codigo);
+    printf("Cidade: %s\n", c2.cidade);
+    printf("Populacao: %d\n", c2.populacao);
+    printf("Area: %.2f Km2\n", c2.area);
+    printf("PIB: %.2f\n", c2.pib);
+    printf("Pontos Turisticos: %d\n", c2.pontos_turisticos);
+
+    if (c1.populacao > c2.populacao) {
+        printf("\nCarta 1 vence na categoria Populacao!\n");
+    } else if (c1.populacao < c2.populacao) {
+        printf("\nCarta 2 vence na categoria Populacao!\n");
+    } else {
+        printf("\nEmpate na categoria Populacao!\n");
+    }
+
+    if (c1.area > c2.area) {
+        printf("Carta 1 vence na categoria Area!\n");
+    } else if (c1.area < c2.area) {
+        printf("Carta 2 vence na categoria Area!\n");
+    } else {
+        printf("Empate na categoria Area!\n");
+    }
+
+    if (c1.pib > c2.pib) {
+        printf("Carta 1 vence na categoria PIB!\n");
+    } else if (c1.pib < c2.pib) {
+        printf("Carta 2 vence na categoria PIB!\n");
+    } else {
+        printf("Empate na categoria PIB!\n");
+    }
+
+    if (c1.pontos_turisticos > c2.pontos_turisticos) {
+        printf("Carta 1 vence na categoria Pontos Turisticos!\n");
+    } else if (c1.pontos_turisticos < c2.pontos_turisticos) {
+        printf("Carta 2 vence na categoria Pontos Turisticos!\n");
+    } else {
+        printf("Empate na categoria Pontos Turisticos!\n");
+    }
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
-
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
-
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
-
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
-
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
-
     return 0;
 }
+
